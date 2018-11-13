68eb71a45f646a214ef69e3ecf338dac3a475428
Btushabe <brucetushabe@gmail.com>
2018-11-13 10:44:18 +0300

    made changes in test_sendit file [#161766508]

--- test_sendIT.py
+++ test_sendIT.py
@@ -1,9 +1,4 @@
 import pytest
 
-def test_capital_case():
-    assert capital_case('semaphore') == "semaphore"
-
-def test_raises_exception_on_non_string_arguments():
-    with pytest.raises(TypeError):
-        capital_case(9)
-    
+def test_create_parce_order():
+        assert
